179e924e1530191aac3f12a7858f19015c0614ff
Stephen Hicks <sdh33@cornell.edu>
2017-05-19 10:01:18 -0700

    Abstract out card logic.

--- pentaquark.js
+++ pentaquark.js
@@ -1,3 +1,339 @@
+// System:
+// interface Target {
+//   ?Location location();
+//   void remove();
+// }
+// interface Card extends Target {
+//   Target[] drag();
+// }
+//   
+// interface Location extends Target {
+//   add(??);
+// }
+//
+// class CardGroup implements Location {
+//
+// }
+
+// Draggable vs Clickable?
+//  - we only allow dragging individual cards, but
+//    we dblclick groups...
+//  - what if draggable nested inside draggable?!? how to pick outer/inner?
+//    - then need to provide a handle to drag the outer...
+// Broadcast signals -
+//    "who can be dragged", "who can be clicked", "who can accept"
+//    or maybe some can be more localized... return t/f
+
+
+/** @record @extends {Component} */
+class Target {
+  /** @return {?Location} */ location() {}
+  /**
+   * Sets the location internally, does not update the location itself. This
+   * must only be called by {@link Location#add} and {@link Location#remove}.
+   * @param {?Location} location
+   * @return {boolean} True if the update should proceed.
+   */
+  setLocation(location) {}
+}
+
+/** @record @extends {Component} */
+class Location {
+  /**
+   * @param {!Target} target
+   * @return {boolean} True if the target was removed.
+   */
+  remove(target) {}
+
+  /** @return {!Array<!Target>} */
+  children() {}
+
+  /**
+   * @param {!Target} target
+   * @param {!Target|number=} before
+   * @return {boolean} True if the target was added.
+   */
+  add(target, before = undefined) {}
+}
+
+// TODO - allow selection?!? that's a lot harder
+//   - for now, just click/drag single items...
+
+/** @record */
+class Component {
+  /** @return {!Element} */ element() {}
+  /** @return {boolean} */ click() {}
+  /** @return {boolean} */ dblClick() {}
+  /** @return {!Array<!Component>} */ drag() {}
+  /**
+   * @param {!Array<!Component>} components
+   * @return {?function()}
+   */
+  drop(components) {}
+}
+
+(function() {
+  const /** !WeakMap<!Element, !Component> */ map = new WeakMap();
+  Component.register =
+      (/** !Element */ element, /** !Component */ component) => {
+        map.set(element, component);
+      };
+  Component.get = (/** !Element */ element) => map.get(element);
+})();
+// top-level event listener simply calls these methods...
+
+/** @abstract @implements {Component} */
+class AbstractComponent {
+  constructor(/** !Element */ element) {
+    /** @private @const */ this.element_ = element;
+    Component.register(element, this);
+  }
+  /** @override */ element() { return this.element_; }
+  /** @override */ click() { return false; }
+  /** @override */ dblClick() { return false; }
+  /** @override */ drag() { return []; }
+  /** @override */ drop(components) { return null; }
+}
+
+/** @abstract @implements {Location} */
+class AbstractLocation extends AstractComponent {
+  constructor(/** !Element */ element) {
+    super(element);
+    /** @private @const {!Array<!Target>} */ this.children_ = [];
+  }
+
+  /** @protected */
+  removeChildElement(/** !Element */ element) {
+    this.element().removeChild(element);
+  }
+
+  /** @protected */
+  appendChildElement(/** !Element */ element) {
+    this.element().appendChild(element);
+  }
+
+  /** @protected */
+  insertBeforeElement(/** !Element */ element, /** !Element */ before) {
+    this.element().insertBefore(element, before);
+  }
+
+  /**
+   * @param {!Target} target
+   * @param {number} before
+   * @return {boolean} True if the target can be added at the position.
+   * @protected
+   */
+  canAdd(/** !Target */ target, /** number */ before) { return true; }
+
+  /**
+   * @param {!Target} target
+   * @return {boolean} True if the target can be removed.
+   * @protected
+   */
+  canRemove(/** !Target */ target) { return true; }
+
+  /** @override */
+  remove(target) {
+    if (this != target.location() ||
+        !this.canRemove(target) ||
+        !target.setLocation(null)) {
+      return false;
+    }
+    const index = this.children_.indexOf(target);
+    if (index < 0) return false;
+    this.removeChildElement(target.element());
+    return !!this.children_.splice(index, 1);
+  }
+
+  /** @override */
+  children() { return this.children_.slice(); }
+
+  /** @override */
+  add(target, before = undefined) {
+    if (before == undefined) {
+      before = this.children_.length;
+    } else if (typeof before != 'number') {
+      before = this.children_.indexOf(before);
+      if (before < 0) return false;
+    }
+    if (!this.canAdd(target, before) || !target.setLocation(this)) return false;
+    if (before == this.children_.length) {
+      this.children_.push(target);
+      this.appendChildElement(target.element());
+    } else {
+      this.children_.splice(before, 0, target);
+      this.insertBeforeElement(
+          target.element(), this.children_[before].element());
+    }
+    return true;
+  }
+}
+
+class CardArea extends AbstractLocation {}
+
+/** @abstract @implements {Target} */
+class CardGroup extends AbstractLocation {
+  constructor(/** !Element */ element) {
+    super(element);
+    /** @private */ this.location_ = null;
+  }
+  /** @override */ location() { return this.location_; }
+  /** @override */
+  setLocation(location) {
+    if (!this.canSetLocation(location)) return false;
+    this.location_ = location;
+    return true;
+  }
+  /**
+   * @param {?Location} location
+   * @return {boolean}
+   * @protected
+   */
+  canSetLocation() { return true; }
+  /** @override */
+  canAdd(target, before) { return target instanceof Card; }
+  /** Removes all children and adds directly to parent. */
+  collapse() {
+    let i = 0;
+    while (i < this.children_.length) {
+      const child = this.children_[i];
+      if (this.location().add(child, this)) {
+        thils.children_.splice(i, 1);
+        this.location().remove(this);
+      } else {
+        i++;
+      }
+    }
+  }
+}
+
+/** A `CardGroup` that collapses to empty when it becomes a singleton. */
+class CollapsibleCardGroup extends CardGroup {
+  /** @override */
+  remove(target) {
+    if (super.remove(target) && this.children_.length == 1) this.collapse();
+  }
+}
+
+/** @abstract @implements {Target} */
+class Card extends AbstractComponent {
+  constructor(/** !Element */ element) {
+    super(element);
+    /** @private */ this.location_ = null;
+  }
+
+  /** @override */ location() { return this.location_; }
+  /** @override */
+  setLocation(location) {
+    if (!this.canSetLocation(location)) return false;
+    this.location_ = location;
+    return true;
+  }
+  /**
+   * @param {?Location} location
+   * @return {boolean}
+   * @protected
+   */
+  canSetLocation() { return true; }
+}
+
+(() => {
+  // Let's figure out how to do drag-and-drop...?
+  let dragging = []; // [...Target]
+  let origin = []; // [number, number]
+  let dropTarget = null; // ?Element
+  let drop = null; // ?function()
+
+  function reset() {
+    if (!dragging) return;
+    for (const c of dragging) {
+      c.element().style.left = c.element().style.top = '0px';
+      c.element().style.zIndex = 0;
+      c.element().style.pointerEvents = 'auto';
+      c.element().classList.remove('dragging');
+      c.element().classList.remove('dropOk');
+    }
+    document.body.classList.remove('dragging');
+    dragging = [];
+    dropTarget = null;
+    drop = null;
+  }
+
+  function* componets(target) {
+    while (target) {
+      const component = Component.get(target);
+      if (component) yield component;
+      target = target.parentElement;
+    }
+  }
+
+  document.body.addEventListener('dblclick', e => {
+    for (const component of components(e.target)) {
+      if (component.dblClick()) return;
+    }
+  });
+
+  document.body.addEventListener('mousedown', e => {
+    let targets = [];
+    for (const component of components(e.target)) {
+      targets = component.drag();
+      if (targets.length) break;
+    }
+    if (!targets.length) return;
+    reset();
+    dragging = targets;
+    origin = [e.x, e.y];
+    for (const c of dragging) {
+      c.element().style.pointerEvents = 'none';
+      c.element().style.zIndex = 1;
+      c.element().classList.add('dragging');
+      document.body.classList.add('dragging');
+    }
+  });
+
+  document.body.addEventListener('mousemove', e => {
+    if (!dragging.length) return;
+    if (e.target != dropTarget) {
+      dropTarget = e.target;
+      drop = null;
+      for (const c of components(e.target)) {
+        drop = c.drop(dragging);
+        if (drop) break;
+      }
+    }
+    for (const c of dragging) {
+      c.element().style.left = `${e.x - origin[0]}px`;
+      c.element().style.top = `${e.y - origin[1]}px`;
+      c.element().classList.toggle('dropOk', !!dropTarget);
+    }
+  });
+
+  document.body.addEventListener('mouseup', e => {
+    if (!dragging) return;
+    const savedDrop = drop;
+    reset();
+    if (savedDrop) savedDrop();
+  });
+})();
+
+
+// Game-specific implementation...?
+
+
+class Quark extends Card {
+  constructor(/** !Flavor */ flavor, /** !Color */ color, /** boolean */ anti) {
+    super(template('quark'));
+    /** @private @const */ this.flavor_ = flavor;
+    /** @private @const */ this.color_ = color;
+    /** @private */ this.anti_ = anti;
+    // set up the element?
+  }
+
+  // TODO - ability to confine, annihilate, etc...
+  // TODO - color and flavor as numbers for easier math?  signed for anti?
+
+}
+
 // Let's figure out how to do drag-and-drop...?
 
 (() => {
@@ -80,18 +416,19 @@
 })();
 
 
-class Location {
-  constructor() {
-    // a .card-area element (or maybe the deck/annihilated/discard?)
-    this.element = undefined;
-  }
-  add(card) {
-    // depends on which location we are...?
-    // could also add confinements?
-  }
-}
+// class Location {
+//   constructor() {
+//     // a .card-area element (or maybe the deck/annihilated/discard?)
+//     this.element = undefined;
+//   }
+//   add(card) {
+//     // depends on which location we are...?
+//     // could also add confinements?
+//   }
+// }
 
-class Persistenet extends Location {
+// Detector and background
+class Persistent extends Location {
   constructor() {
     super();
     this.members = [];
@@ -106,6 +443,9 @@ class Persistenet extends Location {
           //   --> promise... :-/ (disable rest of UI until selected)
           //   .disabled{pointer-events:none}, then add .enabled to choices?
           //   - or .disabled .card to prevent cascading?
+
+          //   -- just block the whole move, unless added on top of
+          //      a free anti-particle.
 
           // drop a new quark into a stable confinement, kick out same type
           return;
